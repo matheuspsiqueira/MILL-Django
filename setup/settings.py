88f43a83347195f1262f993fdf4fb8ae7c63650d
Matheus Siqueira <matheuspontessiqueira@gmail.com>
2023-08-16 00:19:23 -0300

    .

--- setup/settings.py
+++ setup/settings.py
@@ -12,9 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 import os, sys
 from pathlib import Path
-from dotenv import load_dotenv
 
-load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
